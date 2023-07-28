4206f624da212a9b74422d13e92313987dafe7a2
div-vince <dd.utrera@mlgcl.edu.ph>
2023-07-28 09:37:48 +0800

    first commit

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = [
+            'name' => 'Admin',
+            'email' => 'admin@blog.com',
+            'password' => Hash::make('admin_123'),
+            'role' => 1, 
+        ];
+
+        DB::table('users')->insert($admin);
+    }
+}
---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RolesSeeder extends Seeder
+{
+    public function run()
+    {
+        
+        $roles = [
+            ['name' => 'admin'],
+            ['name' => 'user'],
+        ];
+
+        DB::table('role_as')->insert($roles);
+    }
+}
